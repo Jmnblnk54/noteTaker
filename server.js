711e70f6647827f86d615369896f3761677a9dcb
Joshua Blank <jmnblnk@yahoo.com>
2020-08-21 16:14:02 -0700

    complete first api route

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // Dependencies
 // =============================================================
 var express = require("express");
-var db = require ("../db.json");
+var db = require ("./data/db.json");
 var path = require("path");
 var fs = require ("fs");
 
@@ -10,23 +10,32 @@ console.log(db);
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 
 app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/notes.html"));
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
 app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.get("/api/notes", function(req, res) {
+    fs.readFile("data/db.json", 'utf8', (err, data) => {
+        if (err) throw err;
+        let notes = JSON.parse(data);
+        res.json(notes);
+      });    
 });
 
 // If no matching route is found default to home
 app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
+    res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
 
