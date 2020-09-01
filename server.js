a45867ace16ba93dd256866423e7e41775ac1b1b
Joshua Blank <jmnblnk@yahoo.com>
2020-09-01 09:48:58 -0700

    create delete api route

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 // Dependencies
 // =============================================================
 var express = require("express");
-var db = require ("./data/db.json");
+var db = require("./data/db.json");
 var path = require("path");
-var fs = require ("fs");
+var fs = require("fs");
 
 console.log(db);
 
@@ -17,30 +17,53 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "./public/notes.html"));
+app.get("/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "./public/index.html"));
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
-app.get("/api/notes", function(req, res) {
-    fs.readFile("data/db.json", 'utf8', (err, data) => {
-        if (err) throw err;
-        let notes = JSON.parse(data);
-        res.json(notes);
-      });    
+app.get("/api/notes", function (req, res) {
+  fs.readFile("data/db.json", "utf8", (err, data) => {
+    if (err) throw err;
+    let notes = JSON.parse(data);
+    res.json(notes);
+  });
+});
+
+app.post("/api/notes", function (req, res) {
+  fs.readFile("data/db.json", "utf8", (err, data) => {
+    if (err) throw err;
+    let notes = JSON.parse(data);
+    res.json(notes);
+
+    var newNote = req.body;
+
+    console.log(newNote);
+
+    notes.push(newNote);
+
+    res.json(newNote);
+  });
+});
+
+app.delete("/api/notes/:id", function (req, res) {
+  db.Note.destroy({
+    where: {
+      id: req.params.id,
+    },
+  }).then(function (dbNote) {
+    res.json(dbNote);
+  });
 });
 
 // If no matching route is found default to home
-app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "./public/index.html"));
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
-
-
-
-app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
+app.listen(PORT, function () {
+  console.log("App listening on PORT " + PORT);
 });
